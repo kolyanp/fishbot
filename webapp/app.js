30fb2123f9b1b6a9e803f3737f782a72d7c110e2
Kolyan <pal4ikovsky@ukr.net>
2026-08-14 15:18:32 +0300

    Remove telegram login widget

--- webapp/app.js
+++ webapp/app.js
@@ -1197,15 +1197,11 @@ window.addEventListener('load', () => {
     if (!appState.tgUser) {
         const script = document.createElement('script');
         script.async = true;
-        script.src = "https://telegram.org/js/telegram-widget.js?22";
-        script.setAttribute('data-telegram-login', 'Vlasivkabot'); // <-- Bot username
         script.setAttribute('data-size', 'large');
         script.setAttribute('data-onauth', 'onTelegramWidgetAuth(user)');
         script.setAttribute('data-request-access', 'write');
         
-        const container = document.getElementById('telegram-login-container');
         if (container) {
-            container.appendChild(script);
         }
     }
 });
