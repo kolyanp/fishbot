4fb25200c7c1e6e6e9a0e018ea812c53490326ee
Kolyan <pal4ikovsky@ukr.net>
2026-08-14 14:55:21 +0300

    Update SW cache version

---
+++ webapp/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'fishapp-cache-v2';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Cache hit
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
